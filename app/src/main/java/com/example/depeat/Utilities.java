5c05a9c487934265330d257c7f21c6c26cb289b1
antonino <libertoantonio9804@gmail.com>
2019-01-31 16:08:36 +0100

    utilities add

--- app/src/main/java/com/example/depeat/Utilities.java
+++ app/src/main/java/com/example/depeat/Utilities.java
@@ -1,4 +1,20 @@
 package com.example.depeat;
 
+import android.util.Patterns;
+import android.widget.Toast;
+
 public class Utilities {
+    public static boolean controlEmail(String mail){
+        return Patterns.EMAIL_ADDRESS.matcher(mail).matches();
+    }
+    public static  boolean controlPhone(String phone){
+        return Patterns.PHONE.matcher(phone).matches();
+    }
+    public static  boolean controlPassword(String password){
+        if (password.length() > 6)
+            return true;
+        else
+            return false;
+    }
+
 }
